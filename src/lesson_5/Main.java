56e76645182c7838921011be0225cd255931fb1e
Arsen Saliev <arsensaliev03@gmail.com>
2020-09-03 19:32:55 +0600

    fix: problems logic code

--- src/lesson_5/Main.java
+++ src/lesson_5/Main.java
@@ -9,11 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
         float[] arr = new float[SIZE];
+        float[] arr2 = new float[SIZE];
         addArr(arr);
+        addArr(arr2);
+
+
         formula1(arr);
+        formula2(arr2);
 
-
-        formula2(arr);
+        System.out.println(Arrays.equals(arr, arr2));
     }
 
     public static void addArr(float[] arr) {
@@ -31,15 +35,23 @@ public class Main {
 
     public static void formula2(float[] arr) {
         long startTime = System.currentTimeMillis();
-        float[] a1 = new float[HALF];
-        float[] a2 = new float[HALF];
+        float[] arr1 = new float[HALF];
+        float[] arr2 = new float[HALF];
 
-        System.arraycopy(arr, 0, a1, 0, HALF);
-        System.arraycopy(arr, HALF, a2, 0, HALF);
+        System.arraycopy(arr, 0, arr1, 0, HALF);
+        System.arraycopy(arr, HALF, arr2, 0, HALF);
 
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
 
-        Thread thread1 = read(a1);
-        Thread thread2 = read(a2);
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + (i + HALF) / 5) * Math.cos(0.2f + (i + HALF) / 5) * Math.cos(0.4f + (i + HALF) / 2));
+            }
+        });
 
         thread1.start();
         thread2.start();
@@ -47,8 +59,9 @@ public class Main {
         try {
             thread1.join();
             thread2.join();
-            System.arraycopy(a1, 0, arr, 0, HALF);
-            System.arraycopy(a2, 0, arr, HALF, HALF);
+            System.arraycopy(arr1, 0, arr, 0, HALF);
+            System.arraycopy(arr2, 0, arr, HALF, HALF);
+
 
             long endTime = System.currentTimeMillis();
             System.out.println("Formula2 Total execution time: " + (endTime - startTime) + "ms");
@@ -56,13 +69,5 @@ public class Main {
             e.printStackTrace();
         }
 
-    }
-
-    public static Thread read(float[] arr) {
-        return new Thread(() -> {
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        });
     }
 }
